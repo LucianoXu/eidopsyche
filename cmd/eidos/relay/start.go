4447444ad87573194481792c82d32df8d0d27672
Yingte Xu <lucianoxu@foxmail.com>
2026-05-09 10:58:03 +0200

    feat(relay): eidos relay start
    Foreground command. Loads relaycfg, opens the badger eventstore via
    relayd.New, blocks on ListenAndServe with SIGINT/SIGTERM driving
    graceful shutdown.

---
+++ cmd/eidos/relay/start.go
@@ -0,0 +1,68 @@
+package relay
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/spf13/cobra"
+
+	"github.com/LucianoXu/eidopsyche/internal/relaycfg"
+	"github.com/LucianoXu/eidopsyche/internal/relayd"
+)
+
+var startDir string
+
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "Run the relay in the foreground",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		dir := startDir
+		if dir == "" {
+			d, err := relaycfg.DefaultDir()
+			if err != nil {
+				return err
+			}
+			dir = d
+		}
+		cfg, err := relaycfg.Load(dir)
+		if err != nil {
+			return fmt.Errorf("load relay config: %w (have you run `eidos relay init`?)", err)
+		}
+
+		srv, err := relayd.New(relayd.Config{
+			Mode:           relayd.Mode(cfg.Relay.Mode),
+			Listen:         cfg.Relay.Listen,
+			OwnerHex:       cfg.Relay.OwnerPubkey,
+			TLS:            relayd.TLSConfig{CertFile: cfg.Relay.TLS.CertFile, KeyFile: cfg.Relay.TLS.KeyFile},
+			Auth:           relayd.AuthConfig{Required: cfg.Relay.Auth.Required, ServiceURL: cfg.Relay.Auth.ServiceURL},
+			EventStorePath: relaycfg.EventStorePath(dir),
+		})
+		if err != nil {
+			return err
+		}
+
+		ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+		defer cancel()
+
+		fmt.Fprintf(os.Stderr, "eidos-relay listening on %s mode=%s\n", cfg.Relay.Listen, cfg.Relay.Mode)
+		errc := make(chan error, 1)
+		go func() { errc <- srv.ListenAndServe() }()
+		select {
+		case err := <-errc:
+			return err
+		case <-ctx.Done():
+			shutdown, scancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer scancel()
+			return srv.Shutdown(shutdown)
+		}
+	},
+}
+
+func init() {
+	startCmd.Flags().StringVar(&startDir, "dir", "", "config dir (default: ~/.config/eidos/relay)")
+	rootCmd.AddCommand(startCmd)
+}
